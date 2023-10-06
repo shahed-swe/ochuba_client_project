50ce2f8846928b29065dbfb78005970e00f24f63
Md Shahed Talukder <shahedtalukder51@gmail.com>
2023-10-06 09:09:29 +0600

    new commit with ssl config

--- ochuba/vite.config.js
+++ ochuba/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    'import.meta.env.VITE_BASE_URL': JSON.stringify('http://api.ochuba.com'),
+    'import.meta.env.VITE_BASE_URL': JSON.stringify('https://api.ochuba.com'),
   },
 })
