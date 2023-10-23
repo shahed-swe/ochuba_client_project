00454612b18cc4b1b8b1522a8879e85d408f482c
Md Shahed Talukder <shahedtalukder51@gmail.com>
2023-10-23 14:42:10 +0600

    new commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb://127.0.0.1:27017/ochuba';
+const mongoURI = 'mongodb+srv://shahed:srsas1234DFIN@cluster0.vnbi6.mongodb.net/ochuba?retryWrites=true&w=majority';
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI)
